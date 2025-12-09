63e47d503b6b2e682365c7f5dc878dacba7a76bd
sarishmachinta <sarishmachintha@gmail.com>
2025-12-09 18:56:50 +0530

    Add files via upload

---
+++ air quality index.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    
+    if(x<100)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    } 
+    return 0;
+    }
